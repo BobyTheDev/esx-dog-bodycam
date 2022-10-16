3f3a599bd7c9251d57dc3188f62d74b9a79dd8d0
Boby <boby2014@abv.bg>
2022-10-16 14:39:01 +0300

    Update config.lua

--- dog-bodycam/config.lua
+++ dog-bodycam/config.lua
@@ -1,7 +1,7 @@
 
 Config                      = {}
 
-Config.Command = true -- Should be on in order F2 to works
+Config.Job = true -- Currently not working; update soon
 Config.Notify1 = true -- Alert that your body cam is ON
 Config.Notify2 = true -- Alert that tell you that your body cam is OFF
 Config.Notify3 = true -- Alert that will tell you that you don't have a body cam
